9f1a67e3dc475b6a0731f5bbd09b0e09a05c682c
Michael <michaeljohannpeter@gmail.com>
2022-06-05 15:44:00 +0200

    improve markdownlint style-rb

--- .mdl_style.rb
+++ .mdl_style.rb
@@ -2,3 +2,5 @@ all
 
 # Extend line length, since each sentence should be on a separate line.
 rule 'MD013', :line_length => 99999
+# https://github.com/updownpress/markdown-lint/blob/master/rules/024-no-duplicate-header.md
+rule 'MD024', :siblings_only => true
